a6dc87c7557f2d383317b10fa6e208cff22917a9
高麗和樹 <65592763+koumakazuki@users.noreply.github.com>
2020-06-03 14:49:39 +0900

    課題

---
+++ kadai2-2.py
@@ -0,0 +1,11 @@
+import shutil
+for i in range(1,3):
+    shutil.copy('./img_'+str(i)+'.txt','./test1')
+for i in range(3,5):
+    shutil.copy('./img_'+str(i)+'.txt','./test2')
+for i in range(5,7):
+    shutil.copy('./img_'+str(i)+'.txt','./test3')
+for i in range(7,9):
+    shutil.copy('./img_'+str(i)+'.txt','./test4')
+for i in range(9,11):
+    shutil.copy('./img_'+str(i)+'.txt','./test5')
